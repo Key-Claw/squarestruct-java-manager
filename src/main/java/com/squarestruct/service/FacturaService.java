9721ae1143dd5beab3e50ac55d4d79a52d21afee
chipsentinel <chipsentinel@gmail.com>
2026-05-25 00:09:32 +0200

    feat(service): implementar servicios de negocio

---
+++ src/main/java/com/squarestruct/service/FacturaService.java
@@ -0,0 +1,11 @@
+package com.squarestruct.application.service;
+
+public class FacturaService {
+
+    public void validarFactura() {
+
+        System.out.println("Validación de factura correcta");
+
+    }
+
+}
---
+++ src/main/java/com/squarestruct/service/PresupuestoService.java
@@ -0,0 +1,11 @@
+package com.squarestruct.application.service;
+
+public class PresupuestoService {
+
+    public void validarPresupuesto() {
+
+        System.out.println("Validación de presupuesto correcta");
+
+    }
+
+}
